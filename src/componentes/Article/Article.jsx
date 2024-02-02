ec992902bca63d90633e3363cf16aa9ed0c53123
Lohuama <limalorruama98@gmail.com>
2024-02-02 10:20:16 -0300

    project completed

--- src/componentes/Article/Article.jsx
+++ src/componentes/Article/Article.jsx
@@ -1,9 +1,10 @@
 const Article = ({title, text, tags, image, alt}) => {
-    return <div className="p-5 bg-gray-200 dark:bg-dark-200 sm:rounded-xl sm:shadow-md-lg flex flex-col items-center gap-2">
+    return <div className="alura-card">
+
         <h3 className="text-xl text-alura-200 dark:text-gray-200 font-bold">{title}</h3>
         <div className="w-full flex-row justify-end gap-2 pr-5 hidden sm:flex">
             {
-                tags.map((tag) => <span key={tag} className="bg-alura-100 dark:bg-dark-100 px-4 py-1 rounded-full text-gray-200 font-bold ">{tag}</span> )
+                tags.map((tag) => <span key={tag} className="alura-tag">{tag}</span> )
             }
         </div>
         <div className="grid gap-1">
@@ -11,7 +12,7 @@ const Article = ({title, text, tags, image, alt}) => {
                 text.map((content, index) => 
                     <p 
                     key={index} 
-                    className="text-alura-200 dark:text-gray-400"
+                    className="text-alura-200 dark:text-gray-400 line-clamp-2 sm:line-clamp-none"
                     >
                         {content}
                     </p>
--- src/componentes/Form/Form.jsx
+++ src/componentes/Form/Form.jsx
@@ -1,5 +1,3 @@
-import Input from "../Input/Input"
-
 const Form = ({onSubmit}) => {
     const safeSubmit = event => {
         event.preventDefault()
@@ -9,18 +7,20 @@ const Form = ({onSubmit}) => {
         onSubmit({name, email})
     }
     return <form onSubmit={safeSubmit} className="flex flex-col items-center justify-center h-full -mt-20 gap-10 mx-5">
-        <Input 
+        <input
+            className="alura-input" 
             type="text" 
             required 
             placeholder="Insira seu nome" 
-       
+             
         />
-        <Input 
+        <input
+            className="alura-input" 
             type="email" 
             required
-            placeholder="Insisra seu e-mail" 
+            placeholder="Insisra seu e-mail"
         />
-        <button type="submit" className="bg-alura-100 dark:bg-dark-200 rounded-full text-gray-200 py-1 px-5 uppercase outline-non  w-full max-w-sm">Seguir</button>
+        <button type="submit" className="alura-button">Seguir</button>
     </form>
 }
 
